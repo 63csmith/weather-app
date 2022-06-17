8c55c61e671eb0dc2e657f065de1c34fee668367
63csmith <63csmith@gmail.com>
2022-06-16 21:43:59 -0600

    review of this app

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,24 @@
-var weather = {
-    apiKey: "49226ca17e9331a3d0cbf0efaacfa2b6",
-    fetchWeather: function () {
-        fetch(
-            "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid={API key}"
-        ).then((response) => response.json())
-        .then((data) => console.log(data));
-    }
+let city = $("#city").val();
+
+
+$("#src").click(function(e){
+	getLatLon(e)
+});
+
+async function getLatLon(e) {
+	let city = $("#city").val();
+let response = await fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=5&appid=49226ca17e9331a3d0cbf0efaacfa2b6&units=imperial").then(res => res.json()).then(data => data[0]);
+	let {lat, lon} = response
+	getCurrentWeather(lat, lon);
+}
+
+
+function getCurrentWeather(lat, lon) {
+	var weather =
+		"https://api.openweathermap.org/data/2.5/weather?lat=" +
+		lat +
+		"&lon=" +
+		lon +
+		"&appid=49226ca17e9331a3d0cbf0efaacfa2b6&units=imperial";
+		fetch(weather).then(res => res.json()).then(data => console.log(data));
 }
